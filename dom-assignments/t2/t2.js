99175a83996520362efe41cdbbd83ebd11d5ac74
Abdur <abdura@metropolia.fi>
2023-08-29 23:24:53 +0300

    new stuff

---
+++ dom-assignments/t2/t2.js
@@ -0,0 +1,30 @@
+// array for todo list
+const todoList = [
+  {
+    id: 1,
+    task: 'Learn HTML',
+    completed: true,
+  },
+  {
+    id: 2,
+    task: 'Learn CSS',
+    completed: true,
+  },
+  {
+    id: 3,
+    task: 'Learn JS',
+    completed: false,
+  },
+  {
+    id: 4,
+    task: 'Learn TypeScript',
+    completed: false,
+  },
+  {
+    id: 5,
+    task: 'Learn React',
+    completed: false,
+  },
+];
+
+// add your code here
