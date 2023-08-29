644e6c314ba2094264c72ee19317f76ba74de6bd
Abdur <abdura@metropolia.fi>
2023-08-29 13:37:06 +0300

    update

--- jsPart2/t6/t6.js
+++ jsPart2/t6/t6.js
@@ -1,5 +1,34 @@
 'use strict';
 
-const movieAmount = +prompt('How many movies do you want to rate?');
+const movies = [];
 
-for (let i = 0; i < movieAmount; i++) {}
+let more = true;
+while (more) {
+  const title = prompt('Enter a movie title:');
+  const rating = prompt('Enter a rating for the movie (1-5):');
+  const movie = {title, rating};
+  movies.push(movie);
+  more = confirm('Do you want to enter more movies?');
+}
+
+function sortMovies(a, b) {
+  const sort = b.rating - a.rating;
+  return sort;
+}
+movies.sort(sortMovies);
+
+const tbody = document.getElementById('target');
+for (const movie of movies) {
+  const tr = document.createElement('tr');
+  const td1 = document.createElement('td');
+  td1.innerText = movie.title;
+  tr.appendChild(td1);
+
+  const td2 = document.createElement('td');
+  td2.innerText = movie.rating;
+  tr.appendChild(td2);
+  tbody.appendChild(tr);
+}
+
+const favourite = movies[0];
+document.getElementById('fav').innerText = favourite.title;
