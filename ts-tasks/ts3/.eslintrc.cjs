a49aabd6d5600ebe422349fb1d6682da3494c022
Abdur <abdura@metropolia.fi>
2023-10-18 21:17:52 +0300

    ts3

---
+++ ts-tasks/ts3/.eslintrc.cjs
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['google', 'eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'require-jsdoc': 0,
+  },
+};
---
+++ ts-tasks/ts3/src/variables.ts
@@ -0,0 +1,9 @@
+const apiUrl = 'https://sodexo-webscrape-r73sdlmfxa-lz.a.run.app/api/v1';
+
+const positionOptions = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+export {apiUrl, positionOptions};
